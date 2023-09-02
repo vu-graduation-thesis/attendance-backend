9bc3480ae33516152df8a9d3f6386c53ee875ddf
huuvuno1 <nguyenhuuvuno1@gmail.com>
2023-09-02 17:09:20 +0700

    feat: update env

--- queue/index.js
+++ queue/index.js
@@ -1,26 +1,35 @@
 import Queue from "bull";
 import redis from "redis";
+
 import config from "../config.js";
 
 const redisClient = redis.createClient({
   password: config.redis.password,
   socket: {
     host: config.redis.host,
-    port: config.redis.host
-  }
+    port: config.redis.port,
+  },
 });
 
 (async () => {
   await redisClient.connect().catch(console.error);
-})()
+
+  const subscriber = redisClient.duplicate();
+
+  await subscriber.connect();
+
+  await subscriber.subscribe("training_data", message => {
+    console.log(message);
+  });
+})();
 
 const messageQueue = new Queue("message", {
   redis: {
     host: config.redis.host,
     port: config.redis.port,
-    password: config.redis.password
-  }
+    password: config.redis.password,
+  },
 });
 
-export { redisClient }
+export { redisClient };
 export default messageQueue;
